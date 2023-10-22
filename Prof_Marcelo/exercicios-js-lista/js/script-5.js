845cf626772a5608c6854edeb74d82de7c181a95
Carolaine dos Santos <100321745+carolainesantos@users.noreply.github.com>
2023-10-22 17:40:07 -0300

    Modificando exercicios POO E Web

--- Prof_Marcelo/exercicios-js-lista/js/script-5.js
+++ Prof_Marcelo/exercicios-js-lista/js/script-5.js
@@ -6,59 +6,38 @@ Calcule e imprima:
 ● média das idades dos homens;
 ● média dos pesos das mulheres. */
 
-let totalHomens = 0;
-let totalMulheres = 0;
-let somaIdadeHomens = 0;
-let somaPesoMulheres = 0;
-let contador = 0;
+function coletarDados() {
+  let totalHomens = 0;
+  let totalMulheres = 0;
+  let somaIdadesHomens = 0;
+  let somaPesosMulheres = 0;
 
-function calcularEstatisticas() {
-  const mediaIdadeHomens =
-    totalHomens === 0 ? 0 : somaIdadeHomens / totalHomens;
-  const mediaPesoMulheres =
-    totalMulheres === 0 ? 0 : somaPesoMulheres / totalMulheres;
+  for (let i = 1; i <= 10; i++) {
+    const sexo = prompt(
+      `Pessoa ${i}: Informe o sexo (M para masculino ou F para feminino):`
+    ).toUpperCase();
+    const idade = parseInt(prompt(`Informe a idade da pessoa ${i}:`));
+    const peso = parseFloat(prompt(`Informe o peso da pessoa ${i}:`));
 
-  document.getElementById("totalHomens").textContent = totalHomens;
-  document.getElementById("totalMulheres").textContent = totalMulheres;
-  document.getElementById("mediaIdadeHomens").textContent =
-    mediaIdadeHomens.toFixed(2);
-  document.getElementById("mediaPesoMulheres").textContent =
-    mediaPesoMulheres.toFixed(2);
-}
-
-function receberDadosPessoa() {
-  const idade = prompt(`Informe a idade da pessoa ${contador + 1}:`);
-  const peso = prompt(`Informe o peso da pessoa ${contador + 1}:`);
-  const sexo = prompt(
-    `Informe o sexo da pessoa ${
-      contador + 1
-    } (M para masculino ou F para feminino):`
-  );
-
-  const idadeNumero = parseFloat(idade);
-  const pesoNumero = parseFloat(peso);
-
-  if (!isNaN(idadeNumero) && !isNaN(pesoNumero)) {
-    if (sexo.toLowerCase() === "m") {
+    if (sexo === "M") {
       totalHomens++;
-      somaIdadeHomens += idadeNumero;
-    } else if (sexo.toLowerCase() === "f") {
+      somaIdadesHomens += idade;
+    } else if (sexo === "F") {
       totalMulheres++;
-      somaPesoMulheres += pesoNumero;
+      somaPesosMulheres += peso;
     } else {
-      alert("Sexo inválido. Use 'M' para masculino ou 'F' para feminino.");
+      alert(`Sexo inválido para a pessoa ${i}. Digite 'M' ou 'F'.`);
+      i--;
     }
-
-    contador++;
-
-    if (contador < 10) {
-      receberDadosPessoa();
-    } else {
-      calcularEstatisticas();
-    }
-  } else {
-    alert("Idade e peso devem ser números válidos.");
-    receberDadosPessoa();
   }
+
+  const mediaIdadesHomens =
+    totalHomens > 0 ? somaIdadesHomens / totalHomens : 0;
+  const mediaPesosMulheres =
+    totalMulheres > 0 ? somaPesosMulheres / totalMulheres : 0;
+
+  alert(`Total de Homens: ${totalHomens}
+Total de Mulheres: ${totalMulheres}
+Média das Idades dos Homens: ${mediaIdadesHomens.toFixed(2)}
+Média dos Pesos das Mulheres: ${mediaPesosMulheres.toFixed(2)}`);
 }
-receberDadosPessoa();
